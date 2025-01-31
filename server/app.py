3ae5c54a1750d3e5b82eb4163740ca04da2d216e
elinhorn <elin.horn@hyperisland.se>
2025-01-31 17:14:51 +0100

    Implement letter tracking in word guessing game

--- server/app.py
+++ server/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+right_letters = ""
+wrong_letters = ""
+
 
 @app.route("/")
 def home():
@@ -11,13 +14,34 @@ def home():
 @app.route("/process", methods=["POST"])
 def process():
     user_input = request.form.get("user_input")
-    check_word()
-    return jsonify({"message": f"you entered: {user_input}"})
+    # check if the word is correct if not run progam
+    answer = check_word(user_input)
+    return {"right": f"{answer[0]}", "wrong": f"{answer[1]}"}
 
 
 # Check is input word is the correct word
-def check_word():
-    print("test")
+def check_word(user_input):
+    the_word = "hund"
+
+    for c in user_input:
+        if c in the_word:
+            global right_letters
+            if c not in right_letters:
+                right_letters += c
+        else:
+            global wrong_letters
+            if c not in wrong_letters:
+                wrong_letters += c
+
+    print(f"these are the right letters {right_letters}")
+    return right_letters, wrong_letters
+
+    # if user_input == the_word:
+    #    print("right word!!")
+    #    return "right word!!"
+    # else:
+    #    print("wrong word :( ")
+    #    return "wrong word!! :("
 
 
 if __name__ == "__main__":
